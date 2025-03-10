51758ccdebeefc993b7147b49ae61cb21db199ec
zpatterson <zc235940@dal.ca>
2025-03-10 12:40:42 -0300

    one more fix

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -9,3 +9,15 @@ async function test() {
         console.error('Error fetching recommendations:', error);
     }
 }
+
+document.getElementById("recipeForm").addEventListener("submit", async function(event) {
+    event.preventDefault();
+
+    const diet = document.getElementById("diet").value;
+    const ingredients = document.getElementById("ingredients").value;
+
+    const response = await fetch(`/.netlify/functions/api/getrecipes?diet=${encodeURIComponent(diet)}&ingredients=${encodeURIComponent(ingredients)}`);
+    const data = await response.json();
+
+    console.log(data);
+});
