0db187844bcf2e9cc89acb225b2caf4ee90f37ab
zpatterson <zc235940@dal.ca>
2025-03-09 22:47:05 -0300

    Fix

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,11 +1,8 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    const city = "London";  // You can use any city here for the weather app
     try {
-        // Fetching recommendations from the backend
         const response = await fetch(`/api/recommendations`);
         const data = await response.json();
 
-        // Log the recommendations
         if (data.tracks) {
             console.log('Recommended Songs:');
             data.tracks.forEach(track => {
